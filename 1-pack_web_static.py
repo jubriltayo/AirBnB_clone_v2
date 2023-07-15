59a5ff6b458289f83c0ba1562aaa1072f024af71
Tayo Jubril <jubriltayo@gmail.com>
2023-07-15 13:23:55 +0000

    compressed gzip

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,21 +6,21 @@ from fabric.api import local
 
 
 def do_pack():
-	""" Creates a tar-generated `gzip` archive of web_static folder """
-	dt = datetime.now()
-	file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-							dt.month,
-							dt.day,
-							dt.hour,
-							dt.minute,
-							dt.second)
-	# create directory `versions` if it doesnt exist								
-	if not os.path.isdir("versions"):
-		# create directory on local machine without error if failed
-		if local("mkdir versions").failed:
-			return None
-	# create gzip file using `tar` command
-	if local("tar -czvf {} web_static".format(file)).failed:
-		return None
+    """ Creates a tar-generated `gzip` archive of web_static folder """
+    dt = datetime.now()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                        dt.month,
+                                                        dt.day,
+                                                        dt.hour,
+                                                        dt.minute,
+                                                        dt.second)
+    # create directory `versions` if it doesnt exist								
+    if not os.path.isdir("versions"):
+        # create directory on local machine without error if failed
+        if local("mkdir versions").failed:
+            return None
+    # create gzip file using `tar` command
+    if local("tar -czvf {} web_static".format(file)).failed:
+        return None
 
-	return file
+    return file
