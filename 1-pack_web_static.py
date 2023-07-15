4598b8e37c029817397cd8f3be946649ae1c626d
Tayo Jubril <jubriltayo@gmail.com>
2023-07-15 13:04:58 +0000

    compressed to gzip archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# Fabric script to generate a `.tgz` archive from contents of web_static
+import os
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+	""" Creates a tar-generated `gzip` archive of web_static folder """
+	dt = datetime.now()
+	file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+							dt.month,
+							dt.day,
+							dt.hour,
+							dt.minute,
+							dt.second)
+	# create directory `versions` if it doesnt exist								
+	if not os.path.isdir("versions"):
+		# create directory on local machine without error if failed
+		if local("mkdir versions").failed:
+			return None
+	# create gzip file using `tar` command
+	if local("tar -czvf {} web_static".format(file)).failed:
+		return None
+
+	return file
