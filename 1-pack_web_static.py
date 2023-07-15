852ef85ce1558f74a0c2fc088fef1a203072f339
Tayo Jubril <jubriltayo@gmail.com>
2023-07-15 13:33:38 +0000

    compressed gzip

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,12 +9,12 @@ def do_pack():
     """ Creates a tar-generated `gzip` archive of web_static folder """
     dt = datetime.now()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                        dt.month,
-                                                        dt.day,
-                                                        dt.hour,
-                                                        dt.minute,
-                                                        dt.second)
-    # create directory `versions` if it doesnt exist								
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    # create directory `versions` if it doesnt existi
     if not os.path.isdir("versions"):
         # create directory on local machine without error if failed
         if local("mkdir versions").failed:
