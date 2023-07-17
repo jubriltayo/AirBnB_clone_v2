8ed1593702e3ea413f3831fa5e6053ddf72a147b
Tayo Jubril <jubriltayo@gmail.com>
2023-07-17 17:32:28 +0000

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
+# Fabric script to generate a `.tgz` archive from contents of web_static
+import os
+from datetime import datetime
 from fabric.api import local
-from time import strftime
-from datetime import date
 
 
 def do_pack():
-    """ A script that generates archive the contents of web_static folder"""
-
-    filename = strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
-
-        return "versions/web_static_{}.tgz".format(filename)
-
-    except Exception as e:
+    """ Creates a tar-generated `gzip` archive of web_static folder """
+    dt = datetime.now()
+    a_file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                           dt.month,
+                                                           dt.day,
+                                                           dt.hour,
+                                                           dt.minute,
+                                                           dt.second)
+    """ create directory `versions` if it doesnt exist """
+    if os.path.isdir("versions") is False:
+        # create directory on local machine without error if failed
+        if local("mkdir -p versions").failed is True:
+            return None
+    # create gzip file using `tar` command
+    if local("tar -czvf {} web_static".format(a_file)).failed is True:
         return None
+    return a_file
