be3405e9181d7302ac790ef02961c5df3a26ca39
Tayo Jubril <jubriltayo@gmail.com>
2023-07-17 09:22:41 +0000

    bug fixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # Script that sets up web servers for deployment of web_static
 
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+sudo apt-get update
+sudo apt-get install -y nginx
 
 sudo mkdir -p /data/web_static/releases/test/ # -p create parent dir
 sudo mkdir -p /data/web_static/shared/
@@ -21,6 +20,29 @@ sudo chown -R ubuntu:ubuntu /data/
 
 # append to nginx config file the ffg in server block section using alias
 # to serve the content of data/.../current to hbtn_static
-sudo sed -i '/listen 80 default_server/a location /hbtn_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 https://google.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
 sudo service nginx restart
+
