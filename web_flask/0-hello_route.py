7144c8357321577e37b3dac8e09b02bd295eb2dd
Tayo Jubril <jubriltayo@gmail.com>
2023-07-22 11:20:23 +0100

    task 00

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,11 +8,12 @@ app = Flask(__name__)
 # Disable strict slashes for the entire app
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def display():
-	""" displays `Hello HBNB!` """
-	return "Hello HBNB!"
+    """ displays `Hello HBNB!` """
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
