a7d5269664bc091364b8b1fcba86974b96f44fdc
Tayo Jubril <jubriltayo@gmail.com>
2023-08-01 16:12:04 +0000

    2-deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,36 +4,45 @@ from fabric.api import run, put, env
 
 env.hosts = ["34.207.121.58", "54.237.103.145"]
 
+
 def do_deploy(archive_path):
     try:
         if not os.path.isfile(archive_path):
             return False
 
-        # upload archive to `/tmp/`on web server
+        # Upload archive to `/tmp/` on the web server
         put(archive_path, "/tmp/")
 
-        # extract filename without extension
+        # Extract filename without extension
         archive_name = os.path.basename(archive_path)
-        arc_name_no_ext = archive_name.split(".")[0]
-        print(arc_name_no_ext)
+        fname = archive_name.split(".")[0]
 
-        # create new directory on server
-        server_dir = "/data/web_static/releases/{}".format(arc_name_no_ext)
+        # Create a new directory on the server
+        server_dir = "/data/web_static/releases/{}".format(fname)
         run("mkdir -p {}".format(server_dir))
 
-        # uncompress archive to a new directory on the same server
+        # Uncompress archive to a new directory on the same server
         run("tar -xzf /tmp/{} -C {}".format(archive_name, server_dir))
 
-        # delete archive from old dir ('/tmp/')
+        # Delete archive from the old directory ('/tmp/')
         run("rm -rf /tmp/{}".format(archive_name))
 
-        # delete symbolic link
+        # Move content out of the sub-folder
+        run("sudo mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(fname, fname))
+
+        # Remove the now empty web_static dir
+        run("sudo rm -rf /data/web_static/releases/{}/web_static/"
+            .format(fname))
+
+        # Delete existing symbolic link
         run("rm /data/web_static/current")
 
-        # create a new symbolic link to link to the new file in server_dir
+        # Create a new symbolic link to link to the new file in server_dir
         run("ln -s {} /data/web_static/current".format(server_dir))
 
         return True
 
-    except:
+    except Exception:
         return False
+
